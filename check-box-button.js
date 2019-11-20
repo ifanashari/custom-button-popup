8eb120f80173478d4256b60fc8fc016183e31a7e
ifan <ifanashari1234@gmail.com>
2019-11-20 12:02:35 +0700

    update | global custom-popup-button

--- check-box-button.js
+++ check-box-button.js
@@ -1,90 +1,17 @@
-// ==========><>|Store|<><==========
+var globalButtonTarget = document.querySelector(".c_popup-btn")
+var contentId = null
+var contentTarget = null
 
-var month = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'Mai',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-]
+globalButtonTarget.addEventListener("click", (e) => {
 
-var targetPopUpContainer = null
-var renderContentResult = ''
-var storedValue = {}
-var storedStatus = []
-var valueIndex = null
-var valueCounter = 1
+    contentId = globalButtonTarget.getAttribute("data-target-content")
+    contentTarget = document.getElementById(contentId)
 
-// ==========><>|Store|<><==========
-
-function initiateCustomMontSelector(popUpContainer) {
-    renderContent(popUpContainer)
-}
-
-function renderContent(id) {
-    targetPopUpContainer = document.getElementById(id)
-    valueCounter = 1
-    renderContentResult = ''
-
-    month.forEach((data) => {
-        renderContentResult += `
-            <div class="c_month_selector-item" id="">
-                <input type="checkbox" name="month-selector" onchange="setValue(${valueCounter}, '` + id + `')">
-                <span>${data}</span>
-            </div>
-        `
-
-        valueCounter++
-    })
-
-    if(targetPopUpContainer != null){
-        targetPopUpContainer.innerHTML = renderContentResult
-    }
-
-}
-
-function setValue(value, id) {
-    if(storedValue[id] != undefined){
-        valueIndex = storedValue[id].indexOf(value)
-
-        if(valueIndex >= 0){
-            storedValue[id].splice(valueIndex, 1)
-        } else {
-            storedValue[id].push(value)
-        }
-    } else{
-        storedValue = {
-            ...storedValue,
-            [id]: []
-        }
-
-        storedValue[id].push(value)
-    }
-}
-
-
-function getCustomMontSelectorValue(id) {
-    if(storedValue[id] != undefined){
-        return []
-    } else{
-        return storedValue[id]
-    }
-}
-
-function updateStatus(id) {
-    targetPopUpContainer = document.getElementById(id)
-    if(targetPopUpContainer.classList.contains("c_month_selector-content-active")){
-        targetPopUpContainer.classList.remove("c_month_selector-content-active")
+    if(contentTarget.classList.contains("c_popup-content-active")){
+        console.log('alpha')
+        contentTarget.classList.remove("c_popup-content-active")
     } else {
-        targetPopUpContainer.classList.add("c_month_selector-content-active")
+        contentTarget.classList.add("c_popup-content-active")
+        console.log('bravo')
     }
-
-    console.log()
-}
+})
