1c2daa30bf85b921ad66b87a5e9c600fbc929622
ifan <ifanashari1234@gmail.com>
2019-11-20 14:26:57 +0700

    update | update style and logic

--- check-box-button.js
+++ check-box-button.js
@@ -1,17 +1,18 @@
-var globalButtonTarget = document.querySelector(".c_popup-btn")
+var globalButtonTarget = document.getElementsByClassName("c_popup-btn")
 var contentId = null
 var contentTarget = null
 
-globalButtonTarget.addEventListener("click", (e) => {
+for (let buttonTargetCounter = 0; buttonTargetCounter < globalButtonTarget.length; buttonTargetCounter++) {
+    globalButtonTarget[buttonTargetCounter].addEventListener("click", () => {
+        contentId = globalButtonTarget[buttonTargetCounter].getAttribute("data-target-content")
+        contentTarget = document.getElementById(contentId)
 
-    contentId = globalButtonTarget.getAttribute("data-target-content")
-    contentTarget = document.getElementById(contentId)
-
-    if(contentTarget.classList.contains("c_popup-content-active")){
-        console.log('alpha')
-        contentTarget.classList.remove("c_popup-content-active")
-    } else {
-        contentTarget.classList.add("c_popup-content-active")
-        console.log('bravo')
-    }
-})
+        if(contentTarget.classList.contains("c_popup-content-active")){
+            contentTarget.classList.remove("c_popup-content-active")
+            globalButtonTarget[buttonTargetCounter].classList.remove("c_popup-btn-active")
+        } else {
+            contentTarget.classList.add("c_popup-content-active")
+            globalButtonTarget[buttonTargetCounter].classList.add("c_popup-btn-active")
+        }
+    })
+}
