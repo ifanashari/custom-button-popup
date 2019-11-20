050feb9a6e6754e43b927899228c3869bc3ddb29
ifan <ifanashari1234@gmail.com>
2019-11-20 17:17:44 +0700

    update | new logic

--- check-box-button.js
+++ check-box-button.js
@@ -2,17 +2,30 @@ var globalButtonTarget = document.getElementsByClassName("c_popup-btn")
 var contentId = null
 var contentTarget = null
 
-for (let buttonTargetCounter = 0; buttonTargetCounter < globalButtonTarget.length; buttonTargetCounter++) {
-    globalButtonTarget[buttonTargetCounter].addEventListener("click", () => {
-        contentId = globalButtonTarget[buttonTargetCounter].getAttribute("data-target-content")
-        contentTarget = document.getElementById(contentId)
+var globalPopupContent = document.getElementsByClassName("c_popup-content")
 
-        if(contentTarget.classList.contains("c_popup-content-active")){
-            contentTarget.classList.remove("c_popup-content-active")
-            globalButtonTarget[buttonTargetCounter].classList.remove("c_popup-btn-active")
-        } else {
-            contentTarget.classList.add("c_popup-content-active")
-            globalButtonTarget[buttonTargetCounter].classList.add("c_popup-btn-active")
-        }
+for (let buttonTargetCounter = 0; buttonTargetCounter < globalButtonTarget.length; buttonTargetCounter++) {
+    globalButtonTarget[buttonTargetCounter].addEventListener('focus', function() {
+        closeAllPopupContent()
+        showPopupContent(buttonTargetCounter)
     })
+
+    globalButtonTarget[buttonTargetCounter].addEventListener('blur', function() {
+        closeAllPopupContent(buttonTargetCounter)
+    })
+}
+
+function showPopupContent(buttonTargetCounter) {
+    contentId = globalButtonTarget[buttonTargetCounter].getAttribute("data-target-content")
+    contentTarget = document.getElementById(contentId)
+
+    contentTarget.classList.add("c_popup-content-active")
+    globalButtonTarget[buttonTargetCounter].classList.add("c_popup-btn-active")
+}
+
+function closeAllPopupContent() {
+    for (let i = 0; i < globalPopupContent.length; i++) {
+        globalButtonTarget[i].classList.remove("c_popup-btn-active")
+        globalPopupContent[i].classList.remove('c_popup-content-active')
+    }
 }
